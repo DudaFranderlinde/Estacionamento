a28d99ceeedd5279fee67bfb274a0698c643cb6b
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-05-19 05:49:04 -0300

    Lista de dados mocados

---
+++ src/service/parkList-mock.js
@@ -0,0 +1,51 @@
+export let parkingList = [
+    {
+        "number": 1,
+        "block": 2,
+        "status": "Available",
+        "type": "car",
+        "plate": ""
+    },
+    {
+        "number": 2,
+        "block": 3,
+        "status": "Occupied",
+        "type": "motorcycle",
+        "plate": "ABC-1234"
+    },
+    {
+        "number": 3,
+        "block": 2,
+        "status": "Available",
+        "type": "car",
+        "plate": ""
+    },
+    {
+        "number": 4,
+        "block": 2,
+        "status": "Available",
+        "type": "car",
+        "plate": ""
+    },
+    {
+        "number": 5,
+        "block": 1,
+        "status": "Occupied",
+        "type": "car",
+        "plate": "XYZ-5678"
+    },
+    {
+        "number": 6,
+        "block": 1,
+        "status": "Available",
+        "type": "motorcycle",
+        "plate": ""
+    },
+    {
+        "number": 7,
+        "block": 1,
+        "status": "Occupied",
+        "type": "car",
+        "plate": "DEF-9876"
+    }
+];
