e5882c6377c8cb4593c4b7ce7787006f63f505ca
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-05-19 07:15:27 -0300

    Arrumando lista de dados mocados

--- src/service/parkList-mock.js
+++ src/service/parkList-mock.js
@@ -2,50 +2,71 @@ export let parkingList = [
     {
         "number": 1,
         "block": 2,
-        "status": "Available",
-        "type": "car",
-        "plate": ""
+        "status": "Disponível",
+        "type": "Carro",
+        "plate": "",
+        "color":"",
+        "apartment":null,
+        "owner":""
     },
     {
         "number": 2,
         "block": 3,
-        "status": "Occupied",
-        "type": "motorcycle",
-        "plate": "ABC-1234"
+        "status": "Ocupada",
+        "type": "Moto",
+        "plate": "ABC-1234",
+        "color":"Azul",
+        "apartment":131,
+        "owner":"Jonh"
     },
     {
         "number": 3,
         "block": 2,
-        "status": "Available",
-        "type": "car",
-        "plate": ""
+        "status": "Disponível",
+        "type": "Carro",
+        "plate": "",
+        "color":"",
+        "apartment":null,
+        "owner":""
     },
     {
         "number": 4,
         "block": 2,
-        "status": "Available",
-        "type": "car",
-        "plate": ""
+        "status": "Disponível",
+        "type": "Carro",
+        "plate": "",
+        "color":"",
+        "apartment":null,
+        "owner":""
     },
     {
         "number": 5,
         "block": 1,
-        "status": "Occupied",
-        "type": "car",
-        "plate": "XYZ-5678"
+        "status": "Ocupado",
+        "type": "Carro",
+        "plate": "XYZ-5678",
+        "color":"red",
+        "apartment":101,
+        "owner":"Elle"
     },
     {
         "number": 6,
         "block": 1,
-        "status": "Available",
-        "type": "motorcycle",
-        "plate": ""
+        "status": "Disponível",
+        "type": "Moto",
+        "plate": "",
+        "color":"",
+        "apartment":null,
+        "owner":""
     },
     {
         "number": 7,
         "block": 1,
-        "status": "Occupied",
-        "type": "car",
-        "plate": "DEF-9876"
+        "status": "Ocupado",
+        "type": "Carro",
+        "plate": "DEF-9876",
+        "color":"Preto",
+        "apartment":121,
+        "owner":"Lisa"
     }
 ];
